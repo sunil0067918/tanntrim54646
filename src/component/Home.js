1b586f05c3aa71565921f12bcadbc556a7ddb2e3
sunil <109091779+sunil0067918@users.noreply.github.com>
2024-07-25 18:00:18 +0530

    comit

---
+++ src/component/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import HomePage from './HomePage'
+import Nav from './Nav'
+
+const home = () => {
+  return (
+    <div>
+        <HomePage/>
+        {/* <Nav/>/ */}
+
+
+
+
+    </div>
+  )
+}
+
+export default home
